5c61d73f6e9b9b0d578e709078c076dd267d7d29
Goerig-Florian <florian.goerig@edu.univ-fcomte.fr>
2025-03-13 13:57:00 +0100

    Mise à jour de l'URL de connexion à PocketBase et suppression de fichiers de base de données obsolètes

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -1,5 +1,5 @@
 import PocketBase from 'pocketbase';
-export const pb = new PocketBase(import.meta.env.VITE_URL_POCKETBASE);
+export const pb = new PocketBase('http://127.0.0.1:8090/');
 
 export async function datefilm() {
     const records = await pb.collection('film').getFullList({sort: 'date', expand: 'invite_film'});
